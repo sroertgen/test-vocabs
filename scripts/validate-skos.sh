52026986d030be1a97b071a518717b646a38dc63
Steffen Rörtgen <sroertgen@gmail.com>
2022-11-22 10:47:09 +0100

    add script to validate skos

---
+++ scripts/validate-skos.sh
@@ -0,0 +1,18 @@
+while getopts f:s:l: flag
+do
+    case "${flag}" in
+        f) file=${OPTARG};;
+        s) shape=${OPTARG};;
+        l) severity=${OPTARG};;
+    esac
+done
+
+
+docker run --rm -v $(pwd)/$shape:/rdf/shape.ttl -v $(pwd)/$file:/rdf/file.ttl laocoon667/jena:4.6.1 shacl v -s /rdf/shape.ttl -d /rdf/file.ttl > result.ttl
+validationResult="$(docker run --rm -v $(pwd)/scripts/check-for-violation.rq:/rdf/check-for-violation.rq -v $(pwd)/result.ttl:/rdf/result.ttl laocoon667/jena:4.6.1 arq --data /rdf/result.ttl --query /rdf/check-for-violation.rq)"
+lines=$(echo "$validationResult" | wc -l )
+echo "$validationResult"
+echo $lines
+
+# an empty result, i.e. a correct validation has 4 lines of output
+[[ ${lines} -gt 4 ]] && echo "validation errors" || echo "no validation errors"
