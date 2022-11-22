2a7016966f47b5e9ca1414476378c68911d3ae07
Steffen Rörtgen <sroertgen@gmail.com>
2022-11-22 14:48:43 +0100

    remove shape from repo and instead curl it from shapes repo

--- scripts/validate-skos.sh
+++ scripts/validate-skos.sh
@@ -10,11 +10,14 @@ done
 # fail if any other exit status than 0
 set -e
 
+# download skos shacl shape
+curl https://raw.githubusercontent.com/skohub-io/shapes/main/skos.shacl.ttl --output skos.shacl.ttl
+
 # check file is not empty
 test $(wc -l $file | awk '{print $1}') -gt 0 || (echo "file has no lines, aborting"; exit 1)
 
-echo "Validate: docker run --rm -v $shape:/rdf/shape.ttl -v $file:/rdf/file.ttl laocoon667/jena:4.6.1 shacl v -s /rdf/shape.ttl -d /rdf/file.ttl > result.ttl"
-docker run --rm -v $shape:/rdf/shape.ttl -v $file:/rdf/file.ttl laocoon667/jena:4.6.1 shacl v -s /rdf/shape.ttl -d /rdf/file.ttl > result.ttl
+echo "Validate: docker run --rm -v $(pwd)/skos.shacl.ttl:/rdf/shape.ttl -v $(pwd)/$file:/rdf/file.ttl laocoon667/jena:4.6.1 shacl v -s /rdf/shape.ttl -d /rdf/file.ttl > result.ttl"
+docker run --rm -v $(pwd)/skos.shacl.ttl:/rdf/shape.ttl -v $(pwd)/$file:/rdf/file.ttl laocoon667/jena:4.6.1 shacl v -s /rdf/shape.ttl -d /rdf/file.ttl > result.ttl
 
 # Parse the validation to check for errors
 echo "Parse Validation: docker run --rm -v $(pwd)/scripts/check-for-violation.rq:/rdf/check-for-violation.rq -v $(pwd)/result.ttl:/rdf/result.ttl laocoon667/jena:4.6.1 arq --data /rdf/result.ttl --query /rdf/check-for-violation.rq"
@@ -27,3 +30,6 @@ echo "$validationResult"
 
 # an empty result, i.e. a correct validation has 4 lines of output
 test ${lines} -eq 4 || (echo "validation errors, check output"; exit 1)
+
+# remove created files
+rm result.ttl skos.shacl.ttl
