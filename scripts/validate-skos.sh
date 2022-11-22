6f5744a0f8536c3e86aa614c34d3b60d27a284f9
Steffen Rörtgen <sroertgen@gmail.com>
2022-11-22 13:14:34 +0100

    adjust validation, add more tgest files

--- scripts/validate-skos.sh
+++ scripts/validate-skos.sh
@@ -7,16 +7,14 @@ do
     esac
 done
 
+# fail if any other exit status than 0
+set -e
+
 # check file is not empty
-test $(wc -l $file | awk '{print $1}') -gt 0 || echo "file has no lines, aborting" && exit 1
+test $(wc -l $(pwd)/$file | awk '{print $1}') -gt 0 || (echo "file has no lines, aborting"; exit 1)
 
 echo "Validate: docker run --rm -v $shape:/rdf/shape.ttl -v $file:/rdf/file.ttl laocoon667/jena:4.6.1 shacl v -s /rdf/shape.ttl -d /rdf/file.ttl > result.ttl"
 docker run --rm -v $shape:/rdf/shape.ttl -v $file:/rdf/file.ttl laocoon667/jena:4.6.1 shacl v -s /rdf/shape.ttl -d /rdf/file.ttl > result.ttl
-
-# check exit status for validation
-# if the test file is invalid docker will return a non-zero exit status
-# we should then break
-test $? -eq 0 || echo "something bad happened; maybe riot validation failed, aborting" && exit 1
 
 # Parse the validation to check for errors
 echo "Parse Validation: docker run --rm -v $(pwd)/scripts/check-for-violation.rq:/rdf/check-for-violation.rq -v $(pwd)/result.ttl:/rdf/result.ttl laocoon667/jena:4.6.1 arq --data /rdf/result.ttl --query /rdf/check-for-violation.rq"
@@ -28,4 +26,4 @@ lines=$(echo "$validationResult" | wc -l )
 echo "$validationResult"
 
 # an empty result, i.e. a correct validation has 4 lines of output
-test ${lines} -gt 4 || echo "validation errors" && exit 1
+test ${lines} -eq 4 || (echo "validation errors, check output"; exit 1)
